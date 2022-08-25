b9b2ea4a1f622958e06fc48eab3207117782c9f8
chris chen <christinachen149@gmail.com>
2022-08-26 02:23:23 +0800

    Add html, css

--- dist/assets/js/all.js
+++ dist/assets/js/all.js
@@ -2,5 +2,10 @@
 
 $(function () {
   console.log('Hello Bootstrap5');
+  ClassicEditor.create(document.querySelector('#editor')).then(function (editor) {
+    console.log(editor);
+  })["catch"](function (error) {
+    console.error(error);
+  }); //collapse show
 });
 //# sourceMappingURL=all.js.map
